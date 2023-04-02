7404b7a1950f64f8c327bde0f3aab67cb809e88c
Paty Apaestegui <patyapaesteguicorp@gmail.com>
2023-04-02 17:34:52 -0500

    add oli y luego borrar

---
+++ oli.rb
@@ -0,0 +1,46 @@
+def adsvadv
+
+    asdf
+    asdfasdf
+    asdf
+    eads
+    family
+
+end
+
+def adsvadv
+
+    asdf
+    asdfasdf
+    asdf
+    eads
+    family
+
+end
+def adsvadv
+
+    asdf
+    asdfasdf
+    asdf
+    eads
+    family
+
+end
+def adsvadv
+
+    asdf
+    asdfasdf
+    asdf
+    eads
+    family
+
+end
+def adsvadv
+
+    asdf
+    asdfasdf
+    asdf
+    eads
+    family
+
+end
