e171c3a94d5a035a3b0d661e946a04614064d1b9
Paty Apaestegui <patyapaesteguicorp@gmail.com>
2023-04-04 23:41:09 -0500

    Add: conditional level 1

---
+++ recap.rb
@@ -0,0 +1,61 @@
+######### CHIK TOXICA NIVEL 0 ##############
+# if conditional - si el numero del dado es par
+                    number = rand(6) + 1
+
+                    if number % 2 == 0
+                        puts "el numero #{number} es par"
+                    end
+
+# if else conditional - si el numero de las cartas es mayor a 8
+
+                    
+
+# case when - semáforo
+
+# if 0: rojo - 1:verde - 2:amarillo
+
+                     
+#######################################
+########### CHIKI NIVEL I #############
+#######################################
+
+# imprime del 1 al 10, usando un each
+   
+# imprime del 1 al 10, usando un loop do - break
+                    
+# imprime del 1 al 10, usando un while
+
+
+# imprime del 1 al 10, usando un until
+
+######################################
+########## CHIKI NIVEL II ############
+######################################
+
+# array
+
+# imprime los elementos del array
+
+# duplica los elementos del array
+
+######################################
+###### SUPER CHIKI ###################
+######################################
+# hash: nombres - edades
+
+# imprime las llaves del hash
+
+# imprime los valores del hash
+
+# ordena los elementos del hash segun la edad
+
+#####################################
+######## SUPER CHIKI PLUS 2.0 #######
+#####################################
+# metodos
+# define un metodo que salude greetings
+
+# define un metodo que capitalice el nombre y salude capitalizacion("oliver")
+
+# define un metodo que te diga si el numero es capicua
+
